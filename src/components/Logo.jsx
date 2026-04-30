02cba730cf98e754e40f83f06ccc8b319228944b
Tarunna Sen <tarunnasen@gmail.com>
2026-05-01 02:06:51 +0600

    Footer included

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Logo () {
     return (
-        <Link href="/" className="btn btn-ghost text-xl font-bold">
+        <Link href="/" className="text-xl font-bold">
             Qurbani <span className="text-red-600">Hat</span>
         </Link>
     )
