66bc0d1538176025567d3586f3a697dc0c6922c3
Tarunna Sen <tarunnasen@gmail.com>
2026-04-30 23:15:00 +0600

    changed base url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://fantastic-meme-rj5w9pwj7462x6vx-3000.app.github.dev/"
+    baseURL: "https://qurbani-hat-tarunna.vercel.app/"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
