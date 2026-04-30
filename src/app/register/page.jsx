eb4d387f2979711495102d2fbbf4b97e6f089a0e
Tarunna <tarunnasen@gmail.com>
2026-04-30 16:36:58 +0600

    Registration functionality implemented

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,34 +1,73 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-    const onSubmit = (data) => console.log(data);
+  const handleRegister = async (data) => {
+    const { name, email, photo, password } = data;
+
+    const { data: res, error } = await authClient.signUp.email({
+      name: name,
+      email: email,
+      password: password,
+      image: photo,
+      callbackURL: "/login",
+    });
+
+    if(error) {
+      alert(error.message);
+    }
+    if(res) {
+      alert("Signup Successful");
+    }
+  };
 
   return (
-    <form className="hero mt-20 mb-10" onSubmit={handleSubmit(onSubmit)}>
+    <form className="hero mt-20 mb-10" onSubmit={handleSubmit(handleRegister)}>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-2xl border p-4">
         <h3 className="text-red-500 font-bold text-center text-2xl mb-4">
           Registration Form
         </h3>
 
         <label className="label text-base">Name</label>
-        <input type="text" className="input w-full" {...register("name", { required: true })} placeholder="Name" />
-        {errors.name && <span className="text-red-400">This field is required</span>}
+        <input
+          type="text"
+          className="input w-full"
+          {...register("name", { required: true })}
+          placeholder="Name"
+        />
+        {errors.name && (
+          <span className="text-red-400">This field is required</span>
+        )}
 
         <label className="label text-base">Photo</label>
-        <input type="text" className="input w-full" {...register("photo", { required: true })} placeholder="url" />
-        {errors.photo && <span className="text-red-400">This field is required</span>}
+        <input
+          type="text"
+          className="input w-full"
+          {...register("photo", { required: true })}
+          placeholder="url"
+        />
+        {errors.photo && (
+          <span className="text-red-400">This field is required</span>
+        )}
 
         <label className="label text-base">Email</label>
-        <input type="email" className="input w-full" {...register("email", { required: true })} placeholder="Email" />
-        {errors.email && <span className="text-red-400">This field is required</span>}
+        <input
+          type="email"
+          className="input w-full"
+          {...register("email", { required: true })}
+          placeholder="Email"
+        />
+        {errors.email && (
+          <span className="text-red-400">This field is required</span>
+        )}
 
         <label className="label text-base mt-2">Password</label>
         <input
@@ -37,9 +76,14 @@ export default function LoginPage() {
           {...register("password", { required: true })}
           placeholder="Password"
         />
-        {errors.password && <span className="text-red-400">This field is required</span>}
+        {errors.password && (
+          <span className="text-red-400">This field is required</span>
+        )}
 
-        <button type="submit" className="btn btn-primary bg-red-500 text-white hover:bg-red-600 border-0 mt-8">
+        <button
+          type="submit"
+          className="btn btn-primary bg-red-500 text-white hover:bg-red-600 border-0 mt-8"
+        >
           Register
         </button>
         <div className="text-center">
