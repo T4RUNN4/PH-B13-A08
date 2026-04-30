84cf573f1014ce1d4049bd6a7dba6a0efcf14c84
Tarunna <tarunnasen@gmail.com>
2026-04-30 15:43:26 +0600

    BetterAuth and MongoDB setup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,28 +1,45 @@
-
+"use client";
 import Link from "next/link";
+import { useForm } from "react-hook-form"
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+    const onSubmit = (data) => console.log(data);
+
   return (
-    <form className="hero mt-20">
+    <form className="hero mt-20" onSubmit={handleSubmit(onSubmit)}>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-2xl border p-4">
         <h3 className="text-red-500 font-bold text-center text-2xl mb-4">
           Login Form
         </h3>
 
         <label className="label text-base">Email</label>
-        <input type="email" className="input w-full" placeholder="Email" />
+        <input type="email" className="input w-full" {...register("email", { required: true })} placeholder="Email" />
+        {errors.email && <span className="text-red-400">This field is required</span>}
 
         <label className="label text-base mt-2">Password</label>
         <input
           type="password"
-          className="input w-full mb-4"
+          className="input w-full"
+          {...register("password", { required: true })}
           placeholder="Password"
         />
+        {errors.password && <span className="text-red-400">This field is required</span>}
 
-        <button className="btn btn-primary bg-red-500 text-white hover:bg-red-600 border-0 mt-4">
+        <button type="submit" className="btn btn-primary bg-red-500 text-white hover:bg-red-600 border-0 mt-8">
           Login
         </button>
-        <div className="text-center">New Here? <Link href="/register" className="text-red-600">Register at our platform</Link> </div>
+        <div className="text-center">
+          New Here?{" "}
+          <Link href="/register" className="text-red-600">
+            Register at our platform
+          </Link>{" "}
+        </div>
       </fieldset>
     </form>
   );
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,31 +1,54 @@
+"use client";
 import Link from "next/link";
+import { useForm } from "react-hook-form"
 
-export default function RegisterPage() {
-    return (
-        <form className="hero mt-20">
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-2xl border p-4">
-            <h3 className="text-red-500 font-bold text-center text-2xl mb-4">
-              Registration Form
-            </h3>
-    
-            <label className="label text-base">Your Full Name</label>
-            <input type="text" className="input w-full" placeholder="Name" />
+export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
 
-            <label className="label text-base">Your Email Address</label>
-            <input type="email" className="input w-full" placeholder="Email" />
-    
-            <label className="label text-base mt-2">Your Password</label>
-            <input
-              type="password"
-              className="input w-full mb-4"
-              placeholder="Password"
-            />
-    
-            <button className="btn btn-primary bg-red-500 text-white hover:bg-red-600 border-0 mt-4">
-              Login
-            </button>
-            <div className="text-center">Already registered? <Link href="/login" className="text-red-600">Login from here</Link> </div>
-          </fieldset>
-        </form>
-      );
+    const onSubmit = (data) => console.log(data);
+
+  return (
+    <form className="hero mt-20 mb-10" onSubmit={handleSubmit(onSubmit)}>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-2xl border p-4">
+        <h3 className="text-red-500 font-bold text-center text-2xl mb-4">
+          Registration Form
+        </h3>
+
+        <label className="label text-base">Name</label>
+        <input type="text" className="input w-full" {...register("name", { required: true })} placeholder="Name" />
+        {errors.name && <span className="text-red-400">This field is required</span>}
+
+        <label className="label text-base">Photo</label>
+        <input type="text" className="input w-full" {...register("photo", { required: true })} placeholder="url" />
+        {errors.photo && <span className="text-red-400">This field is required</span>}
+
+        <label className="label text-base">Email</label>
+        <input type="email" className="input w-full" {...register("email", { required: true })} placeholder="Email" />
+        {errors.email && <span className="text-red-400">This field is required</span>}
+
+        <label className="label text-base mt-2">Password</label>
+        <input
+          type="password"
+          className="input w-full"
+          {...register("password", { required: true })}
+          placeholder="Password"
+        />
+        {errors.password && <span className="text-red-400">This field is required</span>}
+
+        <button type="submit" className="btn btn-primary bg-red-500 text-white hover:bg-red-600 border-0 mt-8">
+          Register
+        </button>
+        <div className="text-center">
+          Already registered?{" "}
+          <Link href="/register" className="text-red-600">
+            Login from here
+          </Link>{" "}
+        </div>
+      </fieldset>
+    </form>
+  );
 }
