05825a6396217925d503d8673266b0f05b86f928
Tarunna Sen <tarunnasen@gmail.com>
2026-04-30 17:36:50 +0000

    Thu, Apr 30, 2026, 11:36 PM +06:00

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -11,11 +11,11 @@ const Featured = async () => {
   const animals = await animalsFetch();
 
   return (
-    <div className="my-20 flex flex-col items-center justify-center gap-10">
-      <h1 className="text-5xl font-bold text-gray-800">
+    <div className="py-20 flex flex-col items-center justify-center bg-base-200">
+      <h1 className="text-5xl font-bold text-gray-800 mb-2">
         <span className="text-red-500">Featured</span> Animals
       </h1>
-      <p className="text-sm text-gray-700">Top animals of the Hat</p>
+      <p className="text-sm text-gray-700 mb-10">Top animals of the Hat</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {animals.map((animal) => {
           if (animal.isFeatured) {
