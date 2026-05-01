2fe21701e8912b5f261c6c67b50af3e2c868d100
Tarunna Sen <tarunnasen@gmail.com>
2026-05-02 02:25:14 +0600

    Lottie included

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center bg-base-200 min-h-screen">
+    <div className="flex items-center justify-center bg-base-200 py-20">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold text-red-500">404 Not Found!</h1>
-          <p className="pt-3 pb-6">
-            You came too far away from the Hat in the wrong direction.
-          </p>
+          <DotLottieReact src="/404.lottie" loop autoplay />
           <Link
-          href="/"
-          className="btn btn-primary bg-red-500 border-0 text-white hover:bg-red-600"
-        >
-          Return to Qurbani Hat
-        </Link>
+            href="/"
+            className="btn btn-primary bg-red-500 border-0 text-white hover:bg-red-600"
+          >
+            Return to Qurbani Hat
+          </Link>
         </div>
       </div>
     </div>
